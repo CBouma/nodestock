5fec47cba678449b7aa4715b3e24f42abef08df1
CBouma <CBouma@users.noreply.github.com>
2021-02-11 18:04:30 -0800

    Added node monitor package

--- index.js
+++ index.js
@@ -7,4 +7,6 @@ const PORT = process.env.PORT || 5000;
 // Set Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Use `node index.js` in terminal to start listening, or `nodemon index` now that nodemon package is installed for development environment
+
 app.listen(PORT, () => console.log('Server listening on port ' + PORT ));
